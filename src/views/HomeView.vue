245b97730dc5dc8eeb9d942ccbb4915fbe2d5b95
Ana Alice Honorio <ana.alice.honorio@gmail.com>
2025-02-12 20:18:38 -0300

    esboço inicial componentes vazios e rotas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <HeroComp />
+    <AboutMe />
+    <MyProjects />
+    <ContactMe />
+    <FooterCs />
   </main>
 </template>
+
+<script setup lang="ts">
+import HeroComp from '../components/HeroComp.vue'
+import AboutMe from '../components/AboutMe.vue'
+import MyProjects from '../components/MyProjects.vue'
+import ContactMe from '../components/ContactMe.vue'
+import FooterCs from '../components/FooterCs.vue'
+</script>
